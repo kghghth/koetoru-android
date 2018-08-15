94f473d4342c7793c5d94cec9a087deb793d112d
kogepan159 <kogepan6882@yahoo.co.jp>
2018-08-15 23:51:53 +0900

    player作成

--- app/src/main/java/com/example/junyakengo/koetoru/RecodeFragment.kt
+++ app/src/main/java/com/example/junyakengo/koetoru/RecodeFragment.kt
@@ -1,7 +1,5 @@
 package com.example.junyakengo.koetoru
 
-import android.content.Context
-import android.net.Uri
 import android.util.Log
 import android.os.Bundle
 import android.os.Handler
@@ -12,15 +10,20 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import android.media.MediaRecorder
-import kotlin.math.max
-import java.nio.file.Files.exists
-import android.os.Environment
 import android.widget.EditText
 import java.io.IOException
 import java.io.File
 import android.app.AlertDialog
-import java.util.concurrent.Executors
-import kotlin.system.measureTimeMillis
+import android.media.MediaPlayer
+import android.widget.Toast
+import android.media.AudioManager
+import android.widget.Button
+import android.content.res.AssetFileDescriptor
+
+
+
+
+
 
 
 
@@ -43,6 +46,7 @@ class RecodeFragment : Fragment() {
     private var recodingFlag = false
     val FILENAME = "/sdcard/Movies/"
     var recorder: MediaRecorder = MediaRecorder()
+    private var mediaPlayer: MediaPlayer? = null
     var recodeButton: ImageButton? = null
     var playButton: ImageButton? = null
     var editText: EditText? = null
@@ -157,5 +161,72 @@ class RecodeFragment : Fragment() {
             val s = time % 60
             "%1$02d:%2$02d:%3$02d".format(h, m, s)
         }
+    }
+
+    private fun audioSetup(): Boolean {
+        var fileCheck = false
+
+        // インタンスを生成
+        mediaPlayer = MediaPlayer()
+
+        //音楽ファイル名, あるいはパス
+        val filePath = "music.mp3"
+
+        // assetsから mp3 ファイルを読み込み
+        try {
+            getAssets().openFd(filePath).use({ afdescripter ->
+                // MediaPlayerに読み込んだ音楽ファイルを指定
+                mediaPlayer!!.setDataSource(afdescripter.getFileDescriptor(),
+                        afdescripter.getStartOffset(),
+                        afdescripter.getLength())
+                // 音量調整を端末のボタンに任せる
+                setVolumeControlStream(AudioManager.STREAM_MUSIC)
+                mediaPlayer!!.prepare()
+                fileCheck = true
+            })
+        } catch (e1: IOException) {
+            e1.printStackTrace()
+        }
+
+        return fileCheck
+    }
+
+    private fun audioPlay() {
+
+        if (mediaPlayer == null) {
+            // audio ファイルを読出し
+            if (audioSetup()) {
+                Toast.makeText(getApplication(), "Rread audio file", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(getApplication(), "Error: read audio file", Toast.LENGTH_SHORT).show()
+                return
+            }
+        } else {
+            // 繰り返し再生する場合
+            mediaPlayer!!.stop()
+            mediaPlayer!!.reset()
+            // リソースの解放
+            mediaPlayer!!.release()
+        }
+
+        // 再生する
+        mediaPlayer!!.start()
+
+        // 終了を検知するリスナー
+        mediaPlayer!!.setOnCompletionListener(MediaPlayer.OnCompletionListener {
+            Log.d("debug", "end of audio")
+            audioStop()
+        })
+    }
+
+    private fun audioStop() {
+        // 再生終了
+        mediaPlayer!!.stop()
+        // リセット
+        mediaPlayer!!.reset()
+        // リソースの解放
+        mediaPlayer!!.release()
+
+        mediaPlayer = null
     }
 }
