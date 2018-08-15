65f2b7b9517a36aa73df4a4de9bf88b8cc956eb6
kogepan159 <kogepan6882@yahoo.co.jp>
2018-08-15 13:27:03 +0900

    録音成功！

--- app/src/main/java/com/example/junyakengo/koetoru/RecodeFragment.kt
+++ app/src/main/java/com/example/junyakengo/koetoru/RecodeFragment.kt
@@ -2,11 +2,26 @@ package com.example.junyakengo.koetoru
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.fragment_recode.*
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import kotlin.math.max
+import java.nio.file.Files.exists
+import android.os.Environment
+import java.io.IOException
+import java.io.File
+
+
+
+
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -24,9 +39,64 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class RecodeFragment : Fragment() {
+    private var recodingFlag = false
+    val FILENAME = "/sdcard/sampleWav.wav"
+    var recorder: MediaRecorder = MediaRecorder()
+
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+
+        val mainFrame = inflater!!.inflate(R.layout.fragment_recode, container, false)
         // 先ほどのレイアウトをここでViewとして作成します
-        return inflater!!.inflate(R.layout.fragment_recode, container, false)
+        val imageButton = mainFrame.findViewById(R.id.imageButton) as ImageButton
+        imageButton.setOnClickListener{tapStartRecoding()}
+        return mainFrame
+    }
+
+    fun tapStartRecoding() {
+
+        Log.d(tag, "12346789")
+
+        if (recodingFlag) {
+            Log.d(tag, "Stop Action")
+            stopRecording()
+            Log.d(tag, "Stop OK!!!")
+        } else {
+            Log.d(tag, "Start Action")
+            startRecording()
+            Log.d(tag, "OK!!!")
+        }
+    }
+
+
+    fun startRecording() {
+        var mediafile: File? = File(FILENAME)
+        if (mediafile!!.exists()) {
+            //ファイルが存在する場合は削除する
+            mediafile!!.delete()
+        }
+        mediafile = null
+        recorder = MediaRecorder()
+        //マイクからの音声を録音する
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        //ファイルへの出力フォーマット DEFAULTにするとwavが扱えるはず
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT)
+        //音声のエンコーダーも合わせてdefaultにする
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+        //ファイルの保存先を指定
+        recorder.setOutputFile(FILENAME)
+        //録音の準備をする
+        recorder.prepare()
+        //録音開始
+        recorder.start()
+        recodingFlag = true
+    }
+
+    fun stopRecording() {
+        // 録音停止
+        recorder.stop()
+        recorder.reset()
+        recodingFlag = false
     }
 }
